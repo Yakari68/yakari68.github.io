ec5a10385aa991fbdc7278c3c45bc87a3be58c4a
Yakari68 <56361078+Yakari68@users.noreply.github.com>
2025-07-24 23:44:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
-function toggleMenu(){
-    const element = document.getElementById("collapse");
-    element.classList.toggle("collapse-toggle");
-    const btn = document.getElementById("toggle-btn");
-    const hidden = document.getElementById("hidden-menu");
-    const shown = document.getElementById("shown-menu");
-    if (element.classList.contains("collapse-toggle")){
-        hidden.classList.add("hidebtn");
-        hidden.classList.remove("showbtn");
-        shown.classList.remove("hidebtn");
-        shown.classList.add("showbtn");
-        btn.classList.add("rott");
-        console.log("Show menu");
-        console.log("Do a barrel roll");
-    }else{
-        hidden.classList.add("showbtn");
-        hidden.classList.remove("hidebtn");
-        shown.classList.remove("showbtn");
-        shown.classList.add("hidebtn");
-        btn.classList.remove("rott");
-        console.log("Hide menu");
-        console.log("Do a barrel roll");
+function toggleMenu() {
+    const collapse = document.querySelector('.nav-links');
+    const buttonImg = document.querySelector('.toggle-btn img');
+    const navbar = document.querySelector('.navbar');
+    const main = document.querySelector('main');
+    
+    if (collapse.classList.contains('collapse-hide')) {
+        // Montrer l'élément
+        collapse.classList.remove('collapse-hide');
+        buttonImg.style.transform = 'rotate(90deg)';
+        navbar.style.backgroundColor = '#884400bb';
+        main.style.marginTop="1em";
+
+    } else {
+    // Cacher l'élément
+        collapse.classList.add('collapse-hide');
+        buttonImg.style.transform = 'rotate(0deg)';
+        navbar.style.backgroundColor = '#88440000';
+        main.style.marginTop="-2em";
     }
 }
